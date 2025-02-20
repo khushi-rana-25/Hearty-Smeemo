2f2feb1590002d92b6e5387097964e384dfab942
Khushi Rana <diaskhushi2025@gmail.com>
2025-02-20 20:28:11 +0530

    delete files

---
+++ Smeemo.swiftpm/HomePage.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  Smeemo
+//
+//  Created by Khushi Rana on 20/02/25.
+//
+
+import SwiftUI
+
+struct HomePage: View {
+    var body: some View {
+        Text("Me")
+    }
+}
+
+#Preview {
+    HomePage()
+}
